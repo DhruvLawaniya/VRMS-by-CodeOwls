95928a9cf145ddd4857c00eae3a85a8c484e8bd1
DhruvLawaniya <73819794+DhruvLawaniya@users.noreply.github.com>
2021-01-24 13:31:38 +0530

    calculator

--- Modules/main.py
+++ Modules/main.py
@@ -1,7 +1,10 @@
 from tkinter import *
 from PIL import ImageTk,Image
 import videos
-#import notes.py
+import notes
+import games
+import notes
+import calculator
 
 def books() :
     return 0
@@ -33,6 +36,7 @@ books_button = Button(root,text="Books",padx=10,pady=10,fg="blue",bg="#ffffff",c
 videos_button = Button(root,text="Videos",padx=10,pady=10,fg="blue",bg="#ffffff",command=videos)
 games_button = Button(root,text="Games",padx=10,pady=10,fg="blue",bg="#ffffff",command=games)
 notes_button = Button(root,text="Notes",padx=10,pady=10,fg="blue",bg="#ffffff",command= notes)
+calculator_button = Button(root,text="Calculator",padx=10,pady=10,fg="blue",bg="#ffffff",command= calculator)
 
 #pack in grid
 mainlabel1.grid(row=0,column =0)
@@ -41,6 +45,7 @@ books_button.grid(row=1,column=1)
 videos_button.grid(row=1,column=2)
 games_button.grid(row=1,column=4)
 notes_button.grid(row=1,column=6)
+calculator_button.grid(row=1,column=6)
 
 #search box
 
