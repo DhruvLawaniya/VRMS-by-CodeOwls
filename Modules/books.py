1783cf096f4288f4b22ccaaa30a0be36892d4397
DhruvLawaniya <73819794+DhruvLawaniya@users.noreply.github.com>
2021-01-26 14:24:02 +0530

    Update Books.py

--- Modules/books.py
+++ Modules/books.py
@@ -16,7 +16,7 @@ def books() :
        def openwebbook2():
          webbrowser.open(urlbook2,new)
        new=1
-       BFrame_Right = LabelFrame(root, padx=10,pady=10,bg="#344e5c",width=200,height=200)
+       BFrame_Right = LabelFrame(root, padx=100,pady=100,bg="#344e5c",width=200,height=200)
        BFrame_Right.grid(row=2,column=1)  
        #list ece books and setup their info here itself
        urlbook1 = "https://drive.google.com/file/d/1BfJUZc6QZmCjBllSsvdWYu3kMHJLJuFj/view?usp=sharing"
@@ -27,17 +27,76 @@ def books() :
        book1.grid(row=4,column=2)
        book2= Button(BFrame_Right,text="Electronic Devices And Circuits",compound="top",image=img,bg="#344e5c",borderwidth=0,command=openwebbook2)
        book2.grid(row=4,column=3)
+       BFrame_Right.lift()
 
 
     def phypush():
-       BFrame_Right = LabelFrame(root, padx=10,pady=10,bg="#344e5c",width=200,height=200)
-       BFrame_Right.grid(row=2,column=1)
+        #functions
+       def openwebbook1():
+         webbrowser.open(urlbook1,new)
+       def openwebbook2():
+         webbrowser.open(urlbook2,new)
+       new=1
+       BFrame_Right = LabelFrame(root, padx=100,pady=100,bg="#344e5c",width=200,height=200)
+       BFrame_Right.grid(row=2,column=1)  
+       #list ece books and setup their info here itself
+       urlbook1 = "https://drive.google.com/file/d/1aMedhLqIBva1qXTFq4K26mvkZw6ZB3Z-/view?usp=sharing"
+       
+       urlbook2 = "https://drive.google.com/file/d/19AyL4xv-tcStsmWU0HBeANqT2IWdaU9D/view?usp=sharing"
+       urlbook3 = "https://drive.google.com/file/d/1-ozF-lwUS24ZTM3on2FfrJh6uYtiw6ml/view?usp=sharing"
+       book1= Button(BFrame_Right,text="Griffith's Into Electrodynamics",compound="top",image=img,bg="#344e5c",borderwidth=0,command=openwebbook1)
+       book1.grid(row=4,column=2)
+       book2= Button(BFrame_Right,text="Instructors Solution Manual",compound="top",image=img,bg="#344e5c",borderwidth=0,command=openwebbook2)
+       book2.grid(row=4,column=3)
+       BFrame_Right.lift()
     def mathpush():
-       BFrame_Right = LabelFrame(root, padx=10,pady=10,bg="#344e5c",width=200,height=200)
-       BFrame_Right.grid(row=2,column=1)
+        #functions
+       def openwebbook1():
+         webbrowser.open(urlbook1,new)
+       def openwebbook2():
+         webbrowser.open(urlbook2,new)
+       def openwebbook3():
+         webbrowser.open(urlbook3,new)
+       new=1
+       BFrame_Right = LabelFrame(root, padx=100,pady=100,bg="#344e5c",width=200,height=200)
+       BFrame_Right.grid(row=2,column=1)  
+       #list ece books and setup their info here itself
+       urlbook1 = "https://drive.google.com/file/d/1lQOd54h6lS-1uRBSLFh_f3wIrpjDetu2/view?usp=sharing"
+       
+       urlbook2 = "https://drive.google.com/file/d/1F_8YCTVB3f-7I3GwcXE4QasZ5VgX8Xc8/view?usp=sharing"
+       urlbook3 = "https://drive.google.com/file/d/1LtY4BpQq5aBU5q_WjTZoNYEItrzeJdKu/view?usp=sharing"
+       book1= Button(BFrame_Right,text="Introduction To Real Analysis",compound="top",image=img,bg="#344e5c",borderwidth=0,command=openwebbook1)
+       book1.grid(row=4,column=2)
+       book2= Button(BFrame_Right,text="Ross Elementary Analysis",compound="top",image=img,bg="#344e5c",borderwidth=0,command=openwebbook2)
+       book2.grid(row=4,column=3)
+       book3= Button(BFrame_Right,text="Thomas Calculus",compound="top",image=img,bg="#344e5c",borderwidth=0,command=openwebbook3)
+       book3.grid(row=4,column=4)
+       BFrame_Right.lift()
+
     def csepush():
-       BFrame_Right = LabelFrame(root, padx=10,pady=10,bg="#344e5c",width=200,height=200)
-       BFrame_Right.grid(row=2,column=1)
+        #functions
+       def openwebbook1():
+         webbrowser.open(urlbook1,new)
+       def openwebbook2():
+         webbrowser.open(urlbook2,new)
+       def openwebbook3():
+         webbrowser.open(urlbook3,new)
+       new=1
+       BFrame_Right = LabelFrame(root, padx=100,pady=100,bg="#344e5c",width=200,height=200)
+       BFrame_Right.grid(row=2,column=1)  
+       #list ece books and setup their info here itself
+       urlbook1 = "https://drive.google.com/file/d/16WnwpmYBC5ULP5JvprACY6l7u09O9PxX/view?usp=sharing"
+       
+       urlbook2 = "https://drive.google.com/file/d/1ERSe-zEJxj1kut8xN-YgoKC4ZJmGanxs/view?usp=sharing"
+       urlbook3 = "https://drive.google.com/file/d/1haFTD3ptgYbH25ZkgNH9A_BIUFkoi-V6/view?usp=sharing"
+       book1= Button(BFrame_Right,text="Introduction To Computer \nScience Using Python",compound="top",image=img,bg="#344e5c",borderwidth=0,command=openwebbook1)
+       book1.grid(row=4,column=2)
+       book2= Button(BFrame_Right,text="Think CS Python",compound="top",image=img,bg="#344e5c",borderwidth=0,command=openwebbook2)
+       book2.grid(row=4,column=3)
+       book3= Button(BFrame_Right,text="Think Python",compound="top",image=img,bg="#344e5c",borderwidth=0,command=openwebbook3)
+       book3.grid(row=4,column=4)
+       BFrame_Right.lift()
+
     img = ImageTk.PhotoImage(Image.open(r"images\pdf.png"),height=50,width=50)    
     BFrame_left = LabelFrame(root, padx=10,pady=10,bg="#344e5c",width=200,height=200)
     #my button definitions
