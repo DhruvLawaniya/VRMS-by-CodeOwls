784519798939c6df2ccd7586312bad57c782528b
Alan Louis <64409432+Louis-Alan@users.noreply.github.com>
2021-01-26 14:27:18 +0530

    Update main.py

--- Program/main.py
+++ Program/main.py
@@ -10,7 +10,10 @@ root.title("Learning Management System By Code Owls")
 root.configure(bg="#344e5c")
 root.geometry("1000x500")
 
-
+def home():
+    Main.lift()
+    MainLeft.lift()
+    
 def books() :
     b.books(root)
 
@@ -24,24 +27,28 @@ def games() :
     return 0
 
 def note():
-    Phynotes = Button(root,text="EPHY",padx=25.5,pady=10,fg="red",bg="#344e5c",borderwidth=0,command=phy)
-    CSEnotes = Button(root,text="ECSE",padx=27,pady=10,fg="red",bg="#344e5c",borderwidth=0,command=cse)
-    EECEnotes = Button(root,text="EECE",padx=27,pady=10,fg="red",bg="#344e5c",borderwidth=0,command = eece)
-    EMATnotes = Button(root,text="EMAT",padx=25,pady=10,fg="red",bg="#344e5c",borderwidth=0,command = math)
+    LeftFrame = LabelFrame(root,bg="#344e5c",pady=28,borderwidth=0)
+    LeftFrame.grid(row = 3, column = 0,rowspan=10,padx=10)
 
-    Phynotes.grid(row = 3, column = 1)
-    CSEnotes.grid(row = 5, column = 1)
-    EECEnotes.grid(row = 7, column = 1)
-    EMATnotes.grid(row = 9, column = 1)
+    Phynotes = Button(LeftFrame,text="EPHY",padx=10,pady=10,fg="red",bg="#344e5c",borderwidth=0,command=phy)
+    CSEnotes = Button(LeftFrame,text="ECSE",padx=10,pady=10,fg="red",bg="#344e5c",borderwidth=0,command=cse)
+    EECEnotes = Button(LeftFrame,text="EECE",padx=10,pady=10,fg="red",bg="#344e5c",borderwidth=0,command = eece)
+    EMATnotes = Button(LeftFrame,text="EMAT",padx=10,pady=10,fg="red",bg="#344e5c",borderwidth=0,command = math)
 
+    Phynotes.grid(row = 0, column = 1)
+    CSEnotes.grid(row = 2, column = 1)
+    EECEnotes.grid(row = 4, column = 1)
+    EMATnotes.grid(row = 6, column = 1)
+ 
     #blank
     blank = []
-    j = 2
+    j = 1
     for i in range(4):
-        blank.append(Label(root, text=" Trying to make it long ",fg="#344e5c",bg="#344e5c"))
+        blank.append(Label(LeftFrame, text=" Trying to make it long ",fg="#344e5c",bg="#344e5c"))
         blank[i].grid(row = j, column=1)
         j += 2
-    return 0
+
+    LeftFrame.lift()
 
 def phy():
     n.phys(root)
@@ -55,11 +62,17 @@ def eece():
 def cse():
     n.cse(root)
 
-Noteframe = LabelFrame(root, padx=80,pady=75,bg="#344e5c",width=200,height=200)
-Noteframe.grid(row = 3, column = 2,columnspan = 10,rowspan = 15, padx=5,pady=5)
 
 Main = LabelFrame(root, padx=200,pady=20,bg="#344e5c",width=200,height=200)
 Main.grid(row = 3, column = 2,columnspan = 10,rowspan = 10, padx=5,pady=5)
+
+MainLeft = LabelFrame(root,bg="#344e5c",pady=28,borderwidth=0)
+MainLeft.grid(row = 3, column = 0,rowspan=10,padx=10)
+
+blank = []
+for i in range(8):
+    blank.append(Label(MainLeft, text=" Trying to make it long ",fg="#344e5c",bg="#344e5c"))
+    blank[i].grid(row = i, column=1)
 
 
 logo=ImageTk.PhotoImage(Image.open (r"images\redbull.png"),width=10, height =50)
@@ -70,6 +83,7 @@ main.grid(row=0,column=0)
 #Basic Label
 mainlabel1 = Label(root,pady=0,bg="#344e5c")
 mainlabel2 = Label(root, text="",fg="red",bg="#344e5c")
+homebutton = Button(root,text="HOME",padx=10,pady=10,fg="red",bg="#344e5c",borderwidth=0, command=home)
 booksnotes = Button(root,text="Books",padx=10,pady=10,fg="red",bg="#344e5c",borderwidth=0, command=books)
 videosnotes = Button(root,text="Videos",padx=10,pady=10,fg="red",bg="#344e5c",borderwidth=0,command=videos)
 gamesnotes = Button(root,text="Games",padx=10,pady=10,fg="red",bg="#344e5c",borderwidth=0,command=games)
@@ -85,21 +99,12 @@ for i in range(4):
     j += 2
 
 #pack in grid
+homebutton.grid(row=1,column=0)
 mainlabel1.grid(row=0,column =0)
-mainlabel2.grid(row=1,column=0)
 booksnotes.grid(row=1,column=2)
 videosnotes.grid(row=1,column=4)
 gamesnotes.grid(row=1,column=6)
 notesnotes.grid(row=1,column=8)
 calculatornotes.grid(row=1,column=10)
-
-#Blank space ?
-blank = []
-j = 2
-for i in range(4):
-    blank.append(Label(root, text=" Trying to make it long ",fg="#344e5c",bg="#344e5c"))
-    blank[i].grid(row = j, column=1)
-    j += 2
-
 
 root.mainloop()
