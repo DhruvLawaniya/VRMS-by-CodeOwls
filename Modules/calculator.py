bc419783ce18f4006c0a80a18e97131881f4ce5e
AdwitM <>
2021-01-27 17:34:57 +0530

    updated calc
    color color O__O

--- Modules/calculator.py
+++ Modules/calculator.py
@@ -2,7 +2,7 @@ from tkinter import *
 def call() :	
 	root = Tk()
 	root.title("Simple Calculator")
-
+	root.configure(bg="#24292e")
 	e = Entry(root, width=35, borderwidth=5)
 	e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
@@ -70,22 +70,22 @@ def call() :
 
 	# Defining the Buttons
 
-	button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
-	button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
-	button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
-	button_4 = Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
-	button_5 = Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
-	button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
-	button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
-	button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
-	button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
-	button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
-	button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
-	button_equal = Button(root, text="=", padx=91, pady=20, command=button_equal)
-	button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_clear)
-	button_subtract = Button(root, text="-", padx=41, pady=20, command=button_subtract)
-	button_multiply = Button(root, text="*", padx=40, pady=20, command=button_multiply)
-	button_divide = Button(root, text="/", padx=41, pady=20, command=button_divide)
+	button_1 = Button(root, text="1", padx=40, pady=20,fg="white",bg="#1d2125", command=lambda: button_click(1))
+	button_2 = Button(root, text="2", padx=40, pady=20,fg="white",bg="#1d2125", command=lambda: button_click(2))
+	button_3 = Button(root, text="3", padx=40, pady=20,fg="white",bg="#1d2125", command=lambda: button_click(3))
+	button_4 = Button(root, text="4", padx=40, pady=20,fg="white",bg="#1d2125", command=lambda: button_click(4))
+	button_5 = Button(root, text="5", padx=40, pady=20,fg="white",bg="#1d2125", command=lambda: button_click(5))
+	button_6 = Button(root, text="6", padx=40, pady=20,fg="white",bg="#1d2125", command=lambda: button_click(6))
+	button_7 = Button(root, text="7", padx=40, pady=20,fg="white",bg="#1d2125", command=lambda: button_click(7))
+	button_8 = Button(root, text="8", padx=40, pady=20,fg="white",bg="#1d2125", command=lambda: button_click(8))
+	button_9 = Button(root, text="9", padx=40, pady=20,fg="white",bg="#1d2125", command=lambda: button_click(9))
+	button_0 = Button(root, text="0", padx=40, pady=20,fg="white",bg="#1d2125", command=lambda: button_click(0))
+	button_add = Button(root, text="+", padx=39, pady=20, fg="white",bg="#1d2125", command=button_add)
+	button_equal = Button(root, text="=", padx=91, pady=20, fg="white",bg="#1d2125", command=button_equal)
+	button_clear = Button(root, text="Clear", padx=79, pady=20, fg="white",bg="#1d2125", command=button_clear)
+	button_subtract = Button(root, text="-", padx=41, pady=20, fg="white",bg="#1d2125",command=button_subtract)
+	button_multiply = Button(root, text="*", padx=40, pady=20, fg="white",bg="#1d2125", command=button_multiply)
+	button_divide = Button(root, text="/", padx=41, pady=20, fg="white",bg="#1d2125", command=button_divide)
 
 	# Putting the buttons on the screen
 
