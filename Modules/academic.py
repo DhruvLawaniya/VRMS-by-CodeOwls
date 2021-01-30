527d753c5af1e396955849d486d37eb00bebaf82
DhruvLawaniya <73819794+DhruvLawaniya@users.noreply.github.com>
2021-01-30 17:17:04 +0530

    size of frame change

--- Modules/academic.py
+++ Modules/academic.py
@@ -13,7 +13,7 @@ def academic(root) :
        BFrame_Right.grid(row = 3, column = 2,columnspan = 10,rowspan = 15, padx=5,pady=5) 
        #list ece books and setup their info here itself
        urlbook1 = "https://icampus.bennett.edu.in/#/home/v1/stimetable"
-       book1= Button(BFrame_Right,text="TimeTable",compound="top",image=img2,bg="#344e5c",borderwidth=0,command=openwebbook1)
+       book1= Button(BFrame_Right,text="TimeTable",compound="top",image=img3,bg="#344e5c",borderwidth=0,command=openwebbook1)
        book1.grid(row=4,column=2)
        BFrame_Right.lift()
 
@@ -40,7 +40,7 @@ def academic(root) :
        BFrame_Right.grid(row = 3, column = 2,columnspan = 10,rowspan = 15, padx=5,pady=5)  
        #list ece books and setup their info here itself
        urlbook1 = "https://icampus.bennett.edu.in/#/home/v3/feepayment"
-       book1= Button(BFrame_Right,text="Fees Payment",compound="top",image=img2,bg="#344e5c",borderwidth=0,command=openwebbook1)
+       book1= Button(BFrame_Right,text="Fees Payment",compound="top",image=img4,bg="#344e5c",borderwidth=0,command=openwebbook1)
        book1.grid(row=4,column=2)
        BFrame_Right.lift()
 
@@ -60,6 +60,8 @@ def academic(root) :
     BFrame_left = LabelFrame(root,bg="#1d2125",pady=28,borderwidth=0)
     
     img2 = ImageTk.PhotoImage(Image.open(r"images\chrome.png"),height=50,width=50)
+    img3 = ImageTk.PhotoImage(Image.open(r"images\timetable.png"),height=50,width=50)
+    img4 = ImageTk.PhotoImage(Image.open(r"images\fee.png"),height=50,width=50)
     #my button definitions
     timetablebutton=Button(BFrame_left,text="Time-Table",padx=10,pady=10,fg="white",bg="#1d2125",borderwidth=0,command=timetable)
     marksbutton=Button(BFrame_left,text="Marks",padx=10,pady=10,fg="white",bg="#1d2125",borderwidth=0,command=marks)
