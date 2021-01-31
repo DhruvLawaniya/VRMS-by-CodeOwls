ed9614d17c9a62d1260fa6cbf55844adf991bf8b
AdwitM <>
2021-01-31 19:18:43 +0530

    Update academic.py

--- Modules/academic.py
+++ Modules/academic.py
@@ -13,7 +13,7 @@ def academic(root) :
        BFrame_Right.grid(row = 3, column = 2,columnspan = 10,rowspan = 15, padx=5,pady=5) 
        #list ece books and setup their info here itself
        urlbook1 = "https://icampus.bennett.edu.in/#/home/v1/stimetable"
-       book1= Button(BFrame_Right,text="TimeTable",compound="top",image=img3,bg="#344e5c",borderwidth=0,command=openwebbook1)
+       book1= Button(BFrame_Right,text="TimeTable",compound="top",image=img3,bg="#dfdbd8",borderwidth=0,command=openwebbook1)
        book1.grid(row=4,column=2)
        BFrame_Right.lift()
 
@@ -57,16 +57,16 @@ def academic(root) :
        book1.grid(row=4,column=2)
 
     img = ImageTk.PhotoImage(Image.open(r"images\pdf.png"),height=50,width=50)    
-    BFrame_left = LabelFrame(root,bg="#1d2125",pady=28,borderwidth=0)
+    BFrame_left = LabelFrame(root,bg="#24292e",pady=28,borderwidth=0)
     
     img2 = ImageTk.PhotoImage(Image.open(r"images\chrome.png"),height=50,width=50)
     img3 = ImageTk.PhotoImage(Image.open(r"images\timetable.png"),height=50,width=50)
     img4 = ImageTk.PhotoImage(Image.open(r"images\fee.png"),height=50,width=50)
     #my button definitions
-    timetablebutton=Button(BFrame_left,text="Time-Table",padx=10,pady=10,fg="white",bg="#1d2125",borderwidth=0,command=timetable)
-    marksbutton=Button(BFrame_left,text="Marks",padx=10,pady=10,fg="white",bg="#1d2125",borderwidth=0,command=marks)
-    feesbutton=Button(BFrame_left,text="Fees Payment",padx=10,pady=10,fg="white",bg="#1d2125",borderwidth=0,command=fees)
-    cirriculumbutton=Button(BFrame_left,text="Curriculum",padx=10,pady=10,fg="white",bg="#1d2125",borderwidth=0,command=cirriculum)
+    timetablebutton=Button(BFrame_left,text="Time-Table",padx=10,pady=10,fg="white",bg="#24292e",borderwidth=0,command=timetable)
+    marksbutton=Button(BFrame_left,text="Marks",padx=10,pady=10,fg="white",bg="#24292e",borderwidth=0,command=marks)
+    feesbutton=Button(BFrame_left,text="Fees Payment",padx=10,pady=10,fg="white",bg="#24292e",borderwidth=0,command=fees)
+    cirriculumbutton=Button(BFrame_left,text="Curriculum",padx=10,pady=10,fg="white",bg="#24292e",borderwidth=0,command=cirriculum)
 
     #my button grids
     BFrame_left.grid(row = 3, column = 0,rowspan=10,padx=10)
@@ -78,7 +78,7 @@ def academic(root) :
     blank = []
     j = 1
     for i in range(4):
-        blank.append(Label(BFrame_left, text=" Trying to make it long ",fg="#1d2125",bg="#1d2125"))
+        blank.append(Label(BFrame_left, text=" Trying to make it long ",fg="#24292e",bg="#24292e"))
         blank[i].grid(row = j, column=1)
         j += 2
     
