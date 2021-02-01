5bde3086958448ba3889c2986363616423fed200
AdwitM <>
2021-02-01 12:48:22 +0530

    Update weather.py

--- Modules/weather.py
+++ Modules/weather.py
@@ -3,8 +3,7 @@ import requests
 import json 
 import datetime 
 from PIL import ImageTk, Image 
-  
-  
+
 # necessary details 
 root = Tk() 
 root.title("Weather App") 
@@ -21,14 +20,12 @@ month.place(x=100, y=130)
 # Time 
 hour = Label(root, text=dt.strftime('%I:%M %p'),bg='#24292e', font=("bold", 15),fg="white") 
 hour.place(x=10, y=160) 
-  
 
 # City Search 
 city_name = StringVar() 
 city_entry = Entry(root,textvariable=city_name, width=35, borderwidth=5)
 city_entry.insert(0,"Enter a City Name or Zip Code Here") 
 city_entry.grid(row=1, column=0, ipady=10) 
-  
   
 def city_name(): 
   
@@ -61,44 +58,38 @@ def city_name():
     min_temp.configure(text=tempmin)  
     lable_country.configure(text=country) 
     lable_citi.configure(text=citi) 
-  
-  
+
 # Search Bar and Button 
 city_nameButton = Button(root, text="Search", command=city_name,fg="white",bg="#1d2125") 
 city_nameButton.grid(row=1, column=1, padx=5, stick=W+E+N+S) 
   
-  
 # Country  Names and Coordinates 
 lable_citi = Label(root, text="...", width=0,bg='#24292e',fg='white', font=("bold", 15)) 
 lable_citi.place(x=10, y=63) 
-  
+
 lable_country = Label(root, text="...", width=0,bg='#24292e',fg='white', font=("bold", 15)) 
-lable_country.place(x=135, y=63) 
-  
+lable_country.place(x=135, y=63)
+
 # Current Temperature 
-  
 lable_temp = Label(root, text="...", width=0, bg='#24292e',font=("Helvetica", 110), fg='white') 
 lable_temp.place(x=18, y=220) 
   
 # Other temperature details 
-  
 humi = Label(root, text="Humidity: ", width=0,bg='#24292e', fg='white',font=("bold", 15)) 
-humi.place(x=3, y=400) 
-  
+humi.place(x=3, y=400)
+
 lable_humidity = Label(root, text="...", width=0,bg='#24292e', fg='white',font=("bold", 15)) 
 lable_humidity.place(x=107, y=400) 
   
-  
 maxi = Label(root, text="Max. Temp.: ", width=0,bg='#24292e',fg='white', font=("bold", 15)) 
-maxi.place(x=3, y=430) 
-  
+maxi.place(x=3, y=430)
+
 max_temp = Label(root, text="...", width=0, bg='#24292e',fg='white', font=("bold", 15)) 
 max_temp.place(x=128, y=430) 
-  
-  
+   
 mini = Label(root, text="Min. Temp.: ", width=0,fg='white',  bg='#24292e', font=("bold", 15)) 
 mini.place(x=3, y=460) 
-  
+
 min_temp = Label(root, text="...", width=0,bg='#24292e', fg='white',font=("bold", 15)) 
 min_temp.place(x=128, y=460) 
   
