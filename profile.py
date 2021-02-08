d0829d05f1d27294c92f466061e692270cd1020c
DhruvLawaniya <73819794+DhruvLawaniya@users.noreply.github.com>
2021-02-08 15:46:29 +0530

    pfs

--- profile.py
+++ profile.py
@@ -3,17 +3,18 @@ from PIL import ImageTk,Image
 
 import Modules.main2 as ma
 import Modules.register as regi
+import Modules.forget as fg
 root = Tk()
 root.title("VRMS")
-root.iconbitmap("images\codeowls.png")
+root.iconbitmap("images\mainlogo.ico")
 root.configure(bg="#24292e")
-root.geometry("180x300")
+root.geometry("800x500")
 ProfileMain=Label(root)
 def check() :
     us=u.get()
     pas=p.get()
-    us1=""
-    ps1=""
+    us1="Admin"
+    ps1="Admin"
     us2="Dhruv"
     ps2="Dhruv"
     us3="Rahul"
@@ -27,13 +28,13 @@ def check() :
         if(pas==ps1):
             txt="Autorized"
             msg=Label(root,text=txt)
-            msg.grid(row=3,column=0)
+            msg.grid(row=4,column=0)
             ma.call(us1)
     elif(us==us2):
         if(pas==ps2):
             txt="Autorized"
             msg=Label(root,text=txt)
-            msg.grid(row=3,column=0)
+            msg.grid(row=4,column=0)
             ma.call(us2)
         else:
             txt="Invalid Credentials"
@@ -41,14 +42,14 @@ def check() :
         if(pas==ps3):
             txt="Autorized"
             msg=Label(root,text=txt)
-            msg.grid(row=3,column=0)
+            msg.grid(row=4,column=0)
             ma.call(us3)
         
     elif(us==us4):
         if(pas==ps4):
             txt="Autorized"
             msg=Label(root,text=txt)
-            msg.grid(row=3,column=0)
+            msg.grid(row=4,column=0)
             ma.call(us4)
 
         
@@ -56,28 +57,43 @@ def check() :
         if(pas==ps5):
             txt="Autorized"
             msg=Label(root,text=txt)
-            msg.grid(row=3,column=0)
+            msg.grid(row=4,column=0)
             ma.call(us5)
             
 
         
     msg=Label(root,text=txt)
-    msg.grid(row=3,column=0)
+    msg.grid(row=4,column=0)
 
 def rn():
     regi.call()
+def fp ():
+    fg.call()
 #Logo
 logo=ImageTk.PhotoImage(Image.open ("images\codeowls.png"),width=10, height =50)
 main = Label(root,image=logo,borderwidth="0")
-main.grid(row=5,column=0)
-
+#main.grid(row=5,column=0)
+#us=Label(root,text=)
+looogin= Label(root,text="login here")
 u=Entry(root,text="Enter Username")
 p=Entry(root,text="Enter Password",show="*")
 l=Button(root,text="Login",command=check)
-rn= Button(root,text="Register Now", command=rn)
 
-u.grid(row=0,column=0)
-p.grid(row=1,column=0)
-l.grid(row=2,column=0)
-rn.grid(row=3,column=0)
+fp= Button(root,text="Forgot Password ?", command=fp)
+
+
+u.grid(row=1,column=0)
+p.grid(row=2,column=0)
+l.grid(row=3,column=0)
+looogin.grid(row=0,column=0)
+
+fp.grid(row=5,column=0)
+
+
+
+
+
+rn= Label(root,text="Register Now")
+rn.grid(row=0,column=6)
+
 root.mainloop()
